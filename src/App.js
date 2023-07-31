e3b1b8c0b72e055482c7aac7141dc3831be8c6f3
karlie101 <129705716+karlie101@users.noreply.github.com>
2023-07-31 00:26:40 -0500

    Added some js Components and linked everything together within the project structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Forecast.css";
 
-function App() {
+export default function Forecast() {
+  let forecastInfo = {
+    city: "Hattiesburg",
+    temperature: 76,
+    date: "Friday 12:12",
+    description: "Clear with periodic clouds",
+    imgUrl:
+      "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
+    humidity: 88,
+    wind: 6
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Forecast">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-10">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              id="city-input"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-2">
+            <input type="submit" value="Search" class="btn btn-warning w-150" />
+          </div>
+        </div>
+      </form>
+      <div className="elevate">
+        <h1>{forecastInfo.city}</h1>
+        <ul>
+          <li>Last updated: {forecastInfo.date}</li>
+          <li>{forecastInfo.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex weather-temperature">
+            <img
+              src={forecastInfo.imgUrl}
+              alt={forecastInfo.description}
+              id="icon"
+            />
+            <div className="units">
+              <strong>{forecastInfo.temperature}</strong>
+              <span className="breeze">
+                <a href="/">°F</a> | <a href="/">°C</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {forecastInfo.humidity}%</li>
+            <li>Wind: {forecastInfo.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+      <footer>
+        I am Karlie Howard, and I am 
+      <a href="https://github.com/karlie101/react-weather-app-kh"> open-sourced
+      </a> on github.
+      </footer>
     </div>
+    
   );
 }
-
-export default App;
---
+++ src/Forecast.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Forecast() {
+    return (
+        <div className="App">
+            <Forecast />
+        </div>
+
+    )
+}
